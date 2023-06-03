a6039d96245375a26bafff6e0e18fb571edb7a83
JamesKang003 <kangjames5@gmail.com>
2023-06-03 21:24:15 +0900

    connecting to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
 })
+
+// @Module({
+//   // process.env.MONGO_URL 부분은 하드 코딩해도 괜찮다. "mongodb://localhost:27017"
+//   imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
+// })
 export class AppModule {}
